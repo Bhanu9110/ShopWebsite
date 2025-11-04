a75c02358ffac0df4d1f3fadcd30b3428488cb6e
Bhanu Prakash <prakashreddy2306@gmail.com>
2025-11-04 17:42:39 +0530

    Update vite.config.js for Render deployment

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 10000, // required for Render to detect the app
+    host: '0.0.0.0', // make Vite accessible externally
+    allowedHosts: ['shopwebsite-admin.onrender.com'], // allow your Render domain
+  },
+});
