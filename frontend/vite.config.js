a9d5b2a96358b6edc93f15e40327fa2c741a4a10
Bhanu Prakash <prakashreddy2306@gmail.com>
2025-11-04 16:48:47 +0530

    Allow Render host in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['shopwebsite-frontend.onrender.com'],
+    host: '0.0.0.0',
+    port: 10000,
+  },
 })
